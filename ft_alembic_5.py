5564681ba095433f9897e550738306949322ecec
Vicente Lourenco Prista Von Bonhorst <vlourenc@c1r1s1.42lisboa.com>
2026-06-22 13:06:34 +0100

    done

---
+++ ft_alembic_5.py
@@ -0,0 +1,7 @@
+from alchemy import create_air
+
+if __name__ == "__main__":
+    print("=== Alembic 5 ===")
+    print("Accessing the alchemy module "
+          "using 'from alchemy import...' structure")
+    print(f"Testing create_air: {create_air()}")
